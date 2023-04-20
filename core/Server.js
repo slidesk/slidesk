ce6f9d010ef91638c74d79eb2e3ecdee58ec24d2
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-04-20 19:07:29 +0200

    feat: :sparkles: watcher

--- core/Server.js
+++ core/Server.js
@@ -1,10 +1,17 @@
 import http from "http";
 
-export default function Server(html, port) {
-  var server = http.createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-    res.write(html, "utf-8");
-    res.end();
-  });
-  server.listen(port, () => console.log(`🎉 http://localhost:${port}`));
+export default class Server {
+  constructor(html, port) {
+    this.html = html;
+    var server = http.createServer((req, res) => {
+      res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+      res.write(this.html, "utf-8");
+      res.end();
+    });
+    server.listen(port, () => console.log(`🎉 http://localhost:${port}`));
+  }
+
+  setHTML(html) {
+    this.html = html;
+  }
 }
--- index.js
+++ index.js
@@ -3,19 +3,28 @@ import { program } from "commander";
 import Interpreter from "#interpreter";
 import Server from "#server";
 import open from "open";
+import fs from "fs";
 
 program
   .argument("<talk>")
   .option("-p, --port <int>", "port", 1337)
   .description("Convert & present a talk")
   .action((talk, options) => {
-    const babelfish = new Interpreter(`./${talk}/main.tfs`);
-    babelfish
+    let serve;
+    new Interpreter(`./${talk}/main.tfs`)
       .then(async (html) => {
-        new Server(html, options.port);
+        serve = new Server(html, options.port);
         await open(`http://localhost:${options.port}`);
       })
       .catch((err) => console.error(err));
+    fs.watch(talk, { recursive: true }, (eventType, filename) => {
+      console.log(`♻️  ${filename} is ${eventType}`);
+      new Interpreter(`./${talk}/main.tfs`)
+        .then(async (html) => {
+          serve.setHTML(html);
+        })
+        .catch((err) => console.error(err));
+    });
   });
 
 program.parse();
