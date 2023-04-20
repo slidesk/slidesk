885cdecc3805c829afc4422a62c47030aa1d26ba
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-04-20 19:25:21 +0200

    refactor: :recycle: don't repeat code ;)

--- index.js
+++ index.js
@@ -5,25 +5,29 @@ import Server from "#server";
 import open from "open";
 import fs from "fs";
 
+let serve;
+const flow = (talk, init = false, options = {}) => {
+  new Interpreter(`./${talk}/main.tfs`)
+    .then(async (html) => {
+      if (init) {
+        serve = new Server(html, options.port);
+        await open(`http://localhost:${options.port}`);
+      } else {
+        serve.setHTML(html);
+      }
+    })
+    .catch((err) => console.error(err));
+};
+
 program
   .argument("<talk>")
   .option("-p, --port <int>", "port", 1337)
   .description("Convert & present a talk")
   .action((talk, options) => {
-    let serve;
-    new Interpreter(`./${talk}/main.tfs`)
-      .then(async (html) => {
-        serve = new Server(html, options.port);
-        await open(`http://localhost:${options.port}`);
-      })
-      .catch((err) => console.error(err));
+    flow(talk, true, options);
     fs.watch(talk, { recursive: true }, (eventType, filename) => {
       console.log(`♻️  ${filename} is ${eventType}`);
-      new Interpreter(`./${talk}/main.tfs`)
-        .then(async (html) => {
-          serve.setHTML(html);
-        })
-        .catch((err) => console.error(err));
+      flow(talk, false);
     });
   });
 
