ff718c3486d4e3309f07dd190101141cc1ef0651
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-07-30 00:40:55 +0200

    Migrate to Bun (#19)
    * feat: :recycle: move Interpreter in Bun mode
    
    * feat: :recycle: move Server to Bun
    
    * refactor: :recycle: notes to Bun
    
    * docs: :memo: Upgrade README

--- bin/index.js
+++ bin/index.js
@@ -1,11 +1,8 @@
-#!/usr/bin/env node
-/* eslint-disable no-console */
-/* eslint-disable import/no-unresolved */
+#!/usr/bin/env bun
 import { program } from "commander";
 import open from "open";
-import { watch, writeFileSync } from "fs";
-import Interpreter from "#interpreter";
-import Server from "#server";
+import Interpreter from "../src/core/Interpreter";
+import Server from "../src/core/Server";
 
 let server;
 
@@ -13,7 +10,7 @@ const flow = (talk, options = {}, init = false) => {
   Interpreter.convert(`./${talk}/main.sdf`, options)
     .then(async (html) => {
       if (options.save) {
-        writeFileSync(`./${talk}/index.html`, html);
+        Bun.write(`./${talk}/index.html`, html);
       } else if (init) {
         server = new Server(html, options, `${process.cwd()}/${talk}`);
         if (options.open) {
@@ -37,12 +34,12 @@ program
   .description("Convert & present a talk")
   .action((talk, options) => {
     flow(talk, options, true);
-    if (!options.save) {
-      watch(talk, { recursive: true }, (eventType, filename) => {
-        console.log(`♻️  ${filename} is ${eventType}`);
-        flow(talk, options);
-      });
-    }
+    // if (!options.save) {
+    //   watch(talk, { recursive: true }, (eventType, filename) => {
+    //     console.log(`♻️  ${filename} is ${eventType}`);
+    //     flow(talk, options);
+    //   });
+    // }
   });
 
 program.parse();
--- src/core/Server.js
+++ src/core/Server.js
@@ -1,79 +1,84 @@
-/* eslint-disable no-console */
-/* eslint-disable import/no-unresolved */
-import { createServer } from "http";
-import { WebSocketServer } from "ws";
-import { readFile } from "fs";
-import mime from "mime";
-import { speakerView } from "#speaker_view";
+const speakerViewHTML = await Bun.file(
+  import.meta.resolveSync("../templates/speaker-view.html")
+).text();
+const themeCSS = await Bun.file(
+  import.meta.resolveSync("../templates/theme.css")
+).text();
+const faviconSVG = await Bun.file(
+  import.meta.resolveSync("../templates/SD.svg")
+).text();
 
 export default class Server {
   constructor(html, options, path) {
-    this.html = html;
-    this.path = path;
-    this.io = [];
-    const httpServer = createServer((req, res) => {
-      if (req.url === "/") {
-        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        res.write(this.html, "utf-8");
-        res.end();
-      } else if (req.url === "/favicon.svg") {
-        res.writeHead(200, { "Content-Type": "image/svg+xml" });
-        res.write(
-          `<svg xmlns="http://www.w3.org/2000/svg" width="500" height="500" style="shape-rendering:geometricPrecision;text-rendering:geometricPrecision;image-rendering:optimizeQuality;fill-rule:evenodd;clip-rule:evenodd"><path fill="#fefefe" d="M-.5-.5h500v500H-.5V-.5Z" style="opacity:1"/><path fill="#030303" d="M228.5 192.5v5c-8.934.592-17.601-.075-26-2-1.121-4.377-3.788-7.544-8-9.5a4094.203 4094.203 0 0 0-128 0 20.236 20.236 0 0 0-7.5 7.5 169.242 169.242 0 0 0 0 26c1.5 4.167 4.333 7 8.5 8.5l127 1c18.144 3.141 29.644 13.641 34.5 31.5.667 9.667.667 19.333 0 29-3.558 19.559-15.058 31.059-34.5 34.5-43.667.667-87.333.667-131 0-16.993-4.493-27.327-15.326-31-32.5v-6h26c.187 6.687 3.52 10.854 10 12.5 41.667.667 83.333.667 125 0 4.167-1.5 7-4.333 8.5-8.5.667-9 .667-18 0-27-1.892-3.558-4.725-6.058-8.5-7.5l-132-1c-22.03-8.534-32.03-24.701-30-48.5-1.138-24.254 9.862-39.754 33-46.5 43.333-.667 86.667-.667 130 0 19.167 3.334 30.5 14.501 34 33.5Z" style="opacity:1"/><path fill="#323232" d="M420.5 159.5h-150v165a6891.114 6891.114 0 0 1-1-166c50.503-.332 100.836.001 151 1Z" style="opacity:1"/><path fill="#010101" d="M420.5 159.5c23.7 3.528 38.867 16.862 45.5 40a788.234 788.234 0 0 1 1 78c-4.843 26.176-20.343 41.676-46.5 46.5-49.999.5-99.999.667-150 .5v-165h150Z" style="opacity:1"/><path fill="#898989" d="M418.5 185.5h-122v112h122c-40.83.999-81.83 1.332-123 1v-114c41.17-.332 82.17.001 123 1Z" style="opacity:1"/><path fill="#fefefe" d="M418.5 185.5c10.623 1.783 17.789 7.783 21.5 18l.5 37.5c.14 13.566-.36 27.066-1.5 40.5-3.795 9.301-10.628 14.635-20.5 16h-122v-112h122Z" style="opacity:1"/><path fill="#545454" d="M228.5 192.5c.961 1.765 1.295 3.765 1 6h-27v-3c8.399 1.925 17.066 2.592 26 2v-5Z" style="opacity:1"/><path fill="#828282" d="M58.5 285.5h-26v6c-.968-2.107-1.302-4.441-1-7 9.182-.327 18.182.007 27 1Z" style="opacity:1"/></svg>`
+    globalThis.html = html;
+    globalThis.path = path;
+    globalThis.server = Bun.serve({
+      port: options.port,
+      fetch(req) {
+        const url = new URL(req.url);
+        if (url.pathname === "/")
+          return new Response(globalThis.html, {
+            headers: {
+              "Content-Type": "text/html",
+            },
+          });
+        else if (url.pathname === "/favicon.svg")
+          return new Response(faviconSVG, {
+            headers: { "Content-Type": "image/svg+xml" },
+          });
+        else if (url.pathname === "/notes")
+          return new Response(
+            speakerViewHTML
+              .replace(
+                "#SOCKETS#",
+                `window.slidesk.io = new WebSocket("ws://localhost:${options.port}/ws");`
+              )
+              .replace("/* #STYLES# */", themeCSS),
+            {
+              headers: {
+                "Content-Type": "text/html",
+              },
+            }
+          );
+        else if (url.pathname === "/ws")
+          return globalThis.server.upgrade(req)
+            ? undefined
+            : new Response("WebSocket upgrade error", { status: 400 });
+        const file = Bun.file(
+          req.url.match(
+            /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g
+          )
+            ? req.url
+            : `${globalThis.path}${req.url}`
         );
-        res.end();
-      } else if (req.url === "/notes") {
-        res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        res.write(
-          speakerView.replace(
-            "#SOCKETS#",
-            `window.slidesk.io = new WebSocket("ws://localhost:${options.port}");`
-          ),
-          "utf-8"
-        );
-        res.end();
-      } else {
-        const file = req.url.match(
-          /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g
-        )
-          ? req.url
-          : `${this.path}${req.url}`;
-        // eslint-disable-next-line consistent-return
-        readFile(file, (err, data) => {
-          if (err) {
-            res.writeHead(404);
-            return res.end("File not found.");
-          }
-          res.setHeader("Content-Type", mime.getType(file));
-          res.writeHead(200);
-          res.end(data);
-        });
-      }
+        if (file.size !== 0)
+          return new Response(file, {
+            headers: {
+              "Content-Type": file.type,
+            },
+          });
+        return new Response(`404!`);
+      },
+      websocket: {
+        open(ws) {
+          ws.subscribe("slidesk");
+        },
+        message(ws, message) {
+          ws.publish("slidesk", message);
+        },
+        close(ws) {
+          ws.unsubscribe("slidesk");
+        },
+      },
     });
-    const wss = new WebSocketServer({
-      server: httpServer,
-      perMessageDeflate: false,
-    });
-    wss.on("connection", (ws) => {
-      this.io.push(ws);
 
-      ws.on("message", (data) => {
-        this.io.forEach((io) => {
-          io.send(JSON.stringify(JSON.parse(data)));
-        });
-      });
-    });
-    httpServer.listen(options.port, () => {
-      console.log(`📽️\thttp://localhost:${options.port}`);
-      if (options.notes)
-        console.log(`📝\thttp://localhost:${options.port}/notes`);
-    });
+    console.log(`📽️\thttp://localhost:${options.port}`);
+    if (options.notes)
+      console.log(`📝\thttp://localhost:${options.port}/notes`);
   }
 
   setHTML(html) {
-    this.html = html;
-    this.io.forEach((io) => {
-      io.send(JSON.stringify({ action: "reload" }));
-    });
+    globalThis.html = html;
+    globalThis.server.publish("slidesk", { message: "reload" });
   }
 }
