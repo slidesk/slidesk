2f4146e38abb6ec0e0e452a26e71a5be0fe89c80
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-10-18 17:46:01 +0200

    feat: :sparkles: git are relaoded on slide apparition

--- src/components/image.js
+++ src/components/image.js
@@ -2,11 +2,14 @@ export default function image(data) {
   let newData = data;
   [...newData.matchAll(/!image\((.*)\)/g)].forEach((match) => {
     const opts = [...match[1].split("|")];
+    const file = opts[0].trim();
     newData = newData.replace(
       match[0],
-      `<img src="${opts[0].trim()}" ${
+      `<img src="${file}" ${
         opts.length > 1 ? opts[1].trim() : ""
-      } loading="lazy" />`,
+      } loading="lazy" ${
+        file.toLowerCase().endsWith(".gif") ? 'data-gif="1"' : ""
+      } />`,
     );
   });
   return newData;
