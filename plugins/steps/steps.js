7cd6f6315cc15bd2fd59e01c35908a758c289b88
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-11-05 21:20:27 +0100

    LastAction (#78)
    * feat: :sparkles: lastAction
    
    * chore: :bookmark: 2.1.16

--- plugins/steps/steps.js
+++ plugins/steps/steps.js
@@ -1,15 +1,16 @@
 window.slidesk.oldNext = window.slidesk.next;
 
+const checkStepConditions = () =>
+  [...window.slidesk.slides[window.slidesk.currentSlide].classList].includes(
+    "steps",
+  ) && window.slidesk.lastAction === "next";
+
 window.slidesk.prepareSteps = () => {
-  if (
-    [...window.slidesk.slides[window.slidesk.currentSlide].classList].includes(
-      "steps"
-    )
-  ) {
+  if (checkStepConditions()) {
     window.slidesk.step = 0;
     window.slidesk.$lis =
       window.slidesk.slides[window.slidesk.currentSlide].querySelectorAll(
-        "ul li"
+        "ul li",
       );
     window.slidesk.maxSteps = window.slidesk.$lis.length;
     [...window.slidesk.$lis].forEach((li) => li.classList.add("step-hidden"));
@@ -17,16 +18,12 @@ window.slidesk.prepareSteps = () => {
 };
 
 window.slidesk.next = () => {
-  if (
-    [...window.slidesk.slides[window.slidesk.currentSlide].classList].includes(
-      "steps"
-    )
-  ) {
+  if (checkStepConditions()) {
     if (window.slidesk.step === window.slidesk.maxSteps) {
       window.slidesk.oldNext();
     } else {
       window.slidesk.$lis[window.slidesk.step++].classList.remove(
-        "step-hidden"
+        "step-hidden",
       );
     }
   } else window.slidesk.oldNext();
