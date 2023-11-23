e0eae35e15737385cafcc9f1fc124565e4da1b59
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-11-23 15:20:15 +0100

    fix: :bug: link generator

--- src/core/Interpreter.js
+++ src/core/Interpreter.js
@@ -320,14 +320,23 @@ export default class Interpreter {
   };
 
   static #links = (data) => {
-    let htmlData = data;
-    [...htmlData.matchAll(/https?:\/\/(\S*)/g)].forEach((match) => {
-      htmlData = htmlData.replace(
-        match[0],
-        `<a href="${match[0]}" target="_blank" rel="noopener">${match[0]}</a>`,
-      );
-    });
-    return htmlData;
+    const reg = /s?:\/\/([a-zA-Z0-9.-]+(:\d+)?([a-zA-Z0-9._\-/~=?]*\/?))/g;
+    return data
+      .split("http")
+      .map((d, i) => {
+        if (i === 0) return d;
+        if (!d.match(reg)) return `http${d}`;
+        if (d.indexOf('">') !== -1) return `http${d}`;
+        let nd = d;
+        [...nd.matchAll(reg)].forEach((m) => {
+          nd = nd.replace(
+            m[0],
+            `<a href="http${m[0]}" target="_blank" rel="noopener">http${m[0]}</a>`,
+          );
+        });
+        return nd;
+      })
+      .join("");
   };
 
   static #grammar = (data) => {
