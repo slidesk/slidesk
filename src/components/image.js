fdabd0e81290c92c01c3ee22b0f57846050feca4
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-11-27 21:50:42 +0100

    feat: :sparkles: new image management

--- src/components/image.js
+++ src/components/image.js
@@ -1,20 +1,16 @@
 export default function image(data) {
   let newData = data;
   [...newData.matchAll(/!image\((.*)\)/g)].forEach((match) => {
-    const [src, alt, width, height, ...optionals] = [...match[1].split(",")];
+    const [src, alt, width, height, optionals] = [...match[1].split(",")];
     newData = newData.replace(
       match[0],
-      `<div class="sd-img" style="${
-        width && width.trim() !== ""
-          ? `width: ${width.trim()}vw;`
-          : "width: auto;"
+      `<img src="${src.trim()}" loading="lazy" alt="${alt?.trim()}"${
+        width && width.trim() !== "" ? ` init-width="${width.trim()}"` : ""
       }${
-        height && height.trim() !== ""
-          ? `height: ${height.trim()}vh;`
-          : "height: auto;"
-      }${optionals || ""}">
-        <img src="${src.trim()}" loading="lazy" alt="${alt?.trim()}" />
-      </div>`,
+        height && height.trim() !== "" ? ` init-height="${height.trim()}"` : ""
+      }${
+        optionals && optionals.trim() !== "" ? `style="${optionals}"` : ""
+      } />`,
     );
   });
   return newData;
