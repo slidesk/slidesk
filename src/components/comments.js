ac76697e51c9f91ed536956decbf4c067b3f3e33
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-11-28 10:27:10 +0100

    refactor: :recycle: comment parser

--- src/components/comments.js
+++ src/components/comments.js
@@ -1,21 +1,15 @@
 export default function comments(data) {
   let newData = data;
-  const regex = /\/\*([^*]|(\*+[^*/]))*\*\//gm;
-  let m;
-  // eslint-disable-next-line no-cond-assign
-  while ((m = regex.exec(newData)) !== null) {
-    if (m.index === regex.lastIndex) {
-      regex.lastIndex += 1;
-    }
+  [...newData.matchAll(/\/\*([^*]|(\*+[^*/]))*\*\//gm)].forEach((match) => {
     newData = newData.replace(
-      m[0],
-      `<aside class="sd-notes">${m[0]
+      match[0],
+      `<aside class="sd-notes">${match[0]
         .replace("/*", "")
         .replace("*/", "")
         .split("\n")
         .slice(1)
         .join("<br/>")}</aside>`,
     );
-  }
+  });
   return newData;
 }
