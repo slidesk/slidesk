d3b88dfbaf81c440fc150ddd1f9b8aaae7027df3
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-11-28 10:27:24 +0100

    test: :white_check_mark: update tests

--- tests/components/comments.test.js
+++ tests/components/comments.test.js
@@ -6,10 +6,14 @@ const data = `
   /*
   This is a comment
   */
+
+  /*
+  This is a second comment
+  */
 `;
 
 test("comments", () => {
   expect(comments(data)).toEqual(
-    '\n  <aside class="sd-notes">  This is a comment<br/>  </aside>\n',
+    '\n  <aside class="sd-notes">  This is a comment<br/>  </aside>\n\n  <aside class="sd-notes">  This is a second comment<br/>  </aside>\n',
   );
 });
--- tests/components/image.test.js
+++ tests/components/image.test.js
@@ -2,10 +2,10 @@
 import { test, expect } from "bun:test";
 import image from "../../src/components/image";
 
-const data = `!image(myimage.png)`;
+const data = `!image(myimage.png, alt text, 13, 12, float: left;)`;
 
 test("image", () => {
   expect(image(data)).toEqual(
-    '<div class="sd-img" style="width: auto;height: auto;">\n        <img src="myimage.png" loading="lazy" alt="undefined" />\n      </div>',
+    '<div class="sd-img" style=" float: left;" >\n        <img src="myimage.png" loading="lazy" alt="alt text" width="13" height="12" />\n      </div>',
   );
 });
