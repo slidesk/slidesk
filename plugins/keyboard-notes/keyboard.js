85eda0f44f821e3da9f7c06109a47acbc7ea71a4
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-11-29 14:40:43 +0100

    feat: :rocket: plugin keyboard-notes

---
+++ plugins/keyboard-notes/keyboard.js
@@ -0,0 +1,22 @@
+let mapping = {};
+
+const fetchMapping = async () => {
+  try {
+    const response = await fetch("/keyboard.json");
+    const json = await response.json();
+    mapping = json;
+  } catch (erreur) {
+    mapping = {
+      f: "fullscreen",
+    };
+  }
+  document.addEventListener("keydown", (e) => {
+    if (typeof window.slidesk[mapping[e.key]] !== "undefined") {
+      window.slidesk[mapping[e.key]]();
+    } else {
+      window.slidesk.io.send(JSON.stringify({ action: mapping[e.key] }));
+    }
+  });
+};
+
+fetchMapping();
