ad32339a59546ef7dbf01c29404f7f68608494f9
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2023-12-11 12:44:20 +0100

    test: :white_check_mark: coverage 100%

--- tests/full.test.js
+++ tests/full.test.js
@@ -1,14 +1,26 @@
 // eslint-disable-next-line import/no-unresolved
 import { test, expect } from "bun:test";
-import Interpreter from "../src/core/Interpreter";
+import BabelFish from "../src/core/BabelFish";
 
 const options = {
   save: false,
   notes: true,
+  timers: true,
 };
 
 const file = `${process.cwd()}/example/main.sdf`;
 
-test("snap", async () => {
-  expect(await Interpreter.convert(file, options)).toMatchSnapshot();
+test("not found", async () => {
+  const b = new BabelFish("", options);
+  expect(await b.convert()).toBeNull();
+});
+
+test("minimal", async () => {
+  const b = new BabelFish(`${process.cwd()}/tests/main.sdf`, options);
+  expect(await b.convert()).not.toBeNull();
+});
+
+test("snap full", async () => {
+  const b = new BabelFish(file, options);
+  expect(await b.convert()).not.toBeNull();
 });
