7bfbbccdfd23697198545523a6093c3e54eb34bb
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2024-09-15 19:28:56 +0200

    fix: :ambulance: critical hotfix image component

--- src/components/image.ts
+++ src/components/image.ts
@@ -1,28 +1,28 @@
 export default function image(data: string) {
-  let newData = data;
-  [...newData.matchAll(/!image\((.*)\)/g)].forEach((match, _) => {
-    const [src, alt, width, height, optionals, caption] = [
-      ...match[1].split(","),
-    ];
-    let classc = "";
-    if (
-      optionals?.trim().indexOf("[") !== -1 ||
-      optionals?.trim().indexOf(" ") === -1
-    )
-      classc = optionals.trim().replace("[", "").replace("]", "");
-    newData = newData.replace(
-      match[0],
-      `<figure class="sd-img ${classc}"${
-        optionals && classc === "" && optionals.trim() !== ""
-          ? ` style="${optionals}"`
-          : ""
-      } >
+	let newData = data;
+	[...newData.matchAll(/!image\((.*)\)/g)].forEach((match, _) => {
+		const [src, alt, width, height, optionals, caption] = [
+			...match[1].split(","),
+		];
+		let classc = "";
+		if (
+			optionals?.trim().indexOf("[") !== -1 ||
+			optionals?.trim().indexOf(" ") === -1
+		)
+			classc = optionals?.trim().replace("[", "").replace("]", "") || "";
+		newData = newData.replace(
+			match[0],
+			`<figure class="sd-img ${classc}"${
+				optionals && classc === "" && optionals.trim() !== ""
+					? ` style="${optionals}"`
+					: ""
+			} >
         <img src="${src.trim()}" loading="lazy" alt="${alt?.trim()}"${
-          width && width.trim() !== "" ? ` width="${width.trim()}"` : ""
-        }${height && height.trim() !== "" ? ` height="${height.trim()}"` : ""} />
+					width && width.trim() !== "" ? ` width="${width.trim()}"` : ""
+				}${height && height.trim() !== "" ? ` height="${height.trim()}"` : ""} />
         <figcaption ${caption?.trim() === "true" ? "" : "style='display: none;'"}>${alt?.trim()}</figcaption>
       </figure>`,
-    );
-  });
-  return newData;
+		);
+	});
+	return newData;
 }
