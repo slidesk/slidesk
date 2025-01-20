19c02ea7b569d3a256084e7761ee3d618ac1607e
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2025-01-20 21:57:26 +0100

    fix: :bug: fix tmp file from ide

--- tests/components/image.test.js
+++ tests/components/image.test.js
@@ -6,7 +6,7 @@ const data = "!image(myimage.png, alt text, 13, 12, float: left;)";
 
 test("image", () => {
   expect(image(data)).toEqual(
-    "<figure class=\"sd-img \" style=\" float: left;\" >\n        <img src=\"myimage.png\" loading=\"lazy\" alt=\"alt text\" width=\"13\" height=\"12\" />\n        <figcaption style='display: none;'>alt text</figcaption>\n      </figure>",
+    '<figure class="sd-img " style=" float: left;" >\n        <img src="myimage.png" loading="lazy" alt="alt text" width="13" height="12" />\n        <figcaption style=\'display: none;\'>alt text</figcaption>\n      </figure>',
   );
 });
 
@@ -14,7 +14,7 @@ const data2 = "!image(myimage.png, alt text, 13, 12, float)";
 
 test("image", () => {
   expect(image(data2)).toEqual(
-    "<figure class=\"sd-img float\" >\n        <img src=\"myimage.png\" loading=\"lazy\" alt=\"alt text\" width=\"13\" height=\"12\" />\n        <figcaption style='display: none;'>alt text</figcaption>\n      </figure>",
+    '<figure class="sd-img float" >\n        <img src="myimage.png" loading="lazy" alt="alt text" width="13" height="12" />\n        <figcaption style=\'display: none;\'>alt text</figcaption>\n      </figure>',
   );
 });
 
@@ -22,6 +22,6 @@ const data3 = "!image(myimage.png, alt text, 13, 12, [float left])";
 
 test("image", () => {
   expect(image(data3)).toEqual(
-    "<figure class=\"sd-img float left\" >\n        <img src=\"myimage.png\" loading=\"lazy\" alt=\"alt text\" width=\"13\" height=\"12\" />\n        <figcaption style='display: none;'>alt text</figcaption>\n      </figure>",
+    '<figure class="sd-img float left" >\n        <img src="myimage.png" loading="lazy" alt="alt text" width="13" height="12" />\n        <figcaption style=\'display: none;\'>alt text</figcaption>\n      </figure>',
   );
 });
