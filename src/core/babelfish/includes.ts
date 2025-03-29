2139c45e96fe7fab8cdc6f2cb7edfff1fda55574
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2025-03-29 15:23:22 +0100

    feat: better include dirs

--- src/core/babelfish/includes.ts
+++ src/core/babelfish/includes.ts
@@ -1,6 +1,37 @@
 import { readdirSync, lstatSync } from "node:fs";
 import replaceAsync from "../../utils/replaceAsync";
 
+const includeDir = async (file_or_dir: string, p1: string) => {
+  const contents = readdirSync(
+    `${file_or_dir.substring(0, file_or_dir.lastIndexOf("/"))}/${p1}`,
+  );
+  const files = contents
+    .filter((f) => f.endsWith(".sdf"))
+    .sort((a, b) => a.localeCompare(b));
+  const res: string[] = [];
+  for await (const f of files)
+    res.push(
+      await includes(
+        `${file_or_dir.substring(0, file_or_dir.lastIndexOf("/"))}/${p1}/${f}`,
+      ),
+    );
+  const dirs = contents
+    .filter((f) =>
+      lstatSync(
+        `${file_or_dir.substring(0, file_or_dir.lastIndexOf("/"))}/${p1}/${f}`,
+      ).isDirectory(),
+    )
+    .sort((a, b) => a.localeCompare(b));
+  for await (const d of dirs)
+    res.push(
+      await includeDir(
+        `${file_or_dir.substring(0, file_or_dir.lastIndexOf("/"))}/${p1}/${d}`,
+        d,
+      ),
+    );
+  return res.join("\n");
+};
+
 const includes = async (file: string): Promise<string> => {
   const data = await Bun.file(file).text();
   return replaceAsync(`${data}\n`, /\n!include\(([^()]+)\)/g, async (_, p1) => {
@@ -9,19 +40,7 @@ const includes = async (file: string): Promise<string> => {
         `${file.substring(0, file.lastIndexOf("/"))}/${p1}`,
       ).isDirectory()
     ) {
-      const files = readdirSync(
-        `${file.substring(0, file.lastIndexOf("/"))}/${p1}`,
-      )
-        .filter((f) => f.endsWith(".sdf"))
-        .sort((a, b) => a.localeCompare(b));
-      const res: string[] = [];
-      for await (const f of files)
-        res.push(
-          await includes(
-            `${file.substring(0, file.lastIndexOf("/"))}/${p1}/${f}`,
-          ),
-        );
-      return res.join("\n");
+      return includeDir(file, String(p1));
     }
     return includes(`${file.substring(0, file.lastIndexOf("/"))}/${p1}`);
   });
