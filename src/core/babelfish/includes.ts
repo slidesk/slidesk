042fe536057a8d865f99157e09b29ffbcf951e63
Sylvain Gougouzian <sylvain.gougouzian@gmail.com>
2025-10-05 23:05:27 +0200

    feat(includes): add extensions & security

--- src/core/babelfish/includes.ts
+++ src/core/babelfish/includes.ts
@@ -1,12 +1,16 @@
 import { lstatSync, readdirSync } from "node:fs";
 import replaceAsync from "../../utils/replaceAsync";
 
-const includeDir = async (file_or_dir: string, p1: string) => {
+const includeDir = async (
+  file_or_dir: string,
+  p1: string,
+  extensions: string[],
+) => {
   const contents = readdirSync(
     `${file_or_dir.substring(0, file_or_dir.lastIndexOf("/"))}/${p1}`,
   );
   const files = contents
-    .filter((f) => f.endsWith(".sdf"))
+    .filter((file) => extensions.some((ext) => file.endsWith(ext)))
     .sort((a, b) => a.localeCompare(b));
   const res: string[] = [];
   for await (const f of files)
@@ -27,22 +31,30 @@ const includeDir = async (file_or_dir: string, p1: string) => {
       await includeDir(
         `${file_or_dir.substring(0, file_or_dir.lastIndexOf("/"))}/${p1}/${d}`,
         d,
+        extensions,
       ),
     );
   return res.join("\n");
 };
 
 const includes = async (file: string): Promise<string> => {
+  if (!import.meta.resolve(file).includes(process.cwd())) return "";
   const data = await Bun.file(file).text();
   return replaceAsync(`${data}\n`, /\n!include\(([^()]+)\)/g, async (_, p1) => {
+    let [dir, ...exts] = `${p1}`.split(",");
+    if (exts.length === 0) exts = ["sdf"];
     if (
       lstatSync(
-        `${file.substring(0, file.lastIndexOf("/"))}/${p1}`,
+        `${file.substring(0, file.lastIndexOf("/"))}/${dir}`,
       ).isDirectory()
     ) {
-      return includeDir(file, String(p1));
+      return includeDir(
+        file,
+        String(dir),
+        exts.map((e) => e.trim()),
+      );
     }
-    return includes(`${file.substring(0, file.lastIndexOf("/"))}/${p1}`);
+    return includes(`${file.substring(0, file.lastIndexOf("/"))}/${dir}`);
   });
 };
 
